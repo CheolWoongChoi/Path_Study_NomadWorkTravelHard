cbb6e85c2f34835b0a42eb43d80bb99d80915114
CheolWoong Choi <bluesky4381@naver.com>
2024-03-22 07:43:22 +0900

    feat : 3-3 Todos 적용 - Todo 로직 작성

--- App.tsx
+++ App.tsx
@@ -15,9 +15,25 @@ import { useState } from "react";
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [todos, setTodos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload: string) => setText(payload);
+  const addTodo = () => {
+    if (text === "") {
+      return;
+    }
+
+    // save to do
+    const newTodos = Object.assign({}, todos, {
+      [Date.now()]: { text, work: working },
+    });
+    setTodos(newTodos);
+    setText("");
+  };
+
+  // log
+  console.log(todos);
 
   return (
     <View style={styles.container}>
@@ -49,11 +65,12 @@ export default function App() {
           keyboardType="email-address"
           placeholder={working ? "Add a To Do" : "Where do you want to go?"}
           returnKeyLabel="메롱"
-          returnKeyType="send"
+          returnKeyType="done"
           autoCorrect={false}
           style={styles.input}
           value={text}
           onChangeText={onChangeText}
+          onSubmitEditing={addTodo}
         />
       </View>
     </View>
